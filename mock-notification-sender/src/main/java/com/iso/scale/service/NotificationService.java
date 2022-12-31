2b1cb7f03760b90dc5b71f644d512818665a6270
ismet sari <ismet.sari@commencis.com>
2022-12-31 21:13:04 +0300

    fix more

--- mock-notification-sender/src/main/java/com/iso/scale/service/NotificationService.java
+++ mock-notification-sender/src/main/java/com/iso/scale/service/NotificationService.java
@@ -3,16 +3,20 @@ package com.iso.scale.service;
 import javax.validation.constraints.NotNull;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class NotificationService {
 
+    @Value("${server.sleepTime}")
+    private long sleepTime;
+
     public boolean sendNotification(@NotNull final String deviceId) {
 
         try {
-            Thread.sleep(500);
+            Thread.sleep(sleepTime);
         } catch (final InterruptedException ex) {
             Thread.currentThread().interrupt();
             log.error("Error occurred while sleeping in thread");
